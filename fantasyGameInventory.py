a6ddd8424ff1f166589d34889aec6c94f5d5a416
D.J. Crosby <taladan@gmail.com>
2022-01-18 19:07:56 -0500

    Inclusion of addToInventory function
    This function allows a user to add additional inventory items stored in a (single item per entry) list) to an inventory dictionary.

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -17,7 +17,18 @@
 # 1 dagger
 # Total number of items: 62
 
-inventory = {'rope': 1, 'torch': 6, 'god coin': 42, 'dagger': 1, 'arrow': 12}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+goblinLoot = ['copper coin', 'rusty mail armor', 'cracked wooden shield', 'broken lantern', 'copper coin', 'copper coin', 'copper coin']
+
+def addToInventory(inv, addedItems): # Add items intelligently to existing inventory.  This assumes the added items are in a list format with single entries for each item
+    for i in addedItems:
+        if i in inv:
+            inv[i] += 1 # If item exists in inventory, add to it
+        else:
+            inv[i] = 1 # If item isn't in inventory, add the item and the value to 1
+    return inv
+
 
 def displayInventory(inv):
     totalItems = 0
@@ -30,5 +41,11 @@ def displayInventory(inv):
 
 
 if __name__ == '__main__':
+    print("Initial inventory:")
     print(displayInventory(inventory))
-   
+    inventory = addToInventory(inventory, dragonLoot)
+    print("Adding Dragon Loot:")
+    print(displayInventory(inventory))
+    inventory = addToInventory(inventory, goblinLoot)
+    print("Adding Goblin Loot:")
+    print(displayInventory(inventory))
