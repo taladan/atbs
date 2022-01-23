8cfa8e3169714e8d0304cef174ee4d7310717944
D.J. Crosby <taladan@gmail.com>
2022-01-23 15:58:48 -0500

    Updated date_detection.py, now with type hints!

--- date_detection.py
+++ date_detection.py
@@ -15,14 +15,6 @@ dateRegex = re.compile(r'''
     ^([0-3]?\d)/        # Day
     ([01]?\d)/          # Month
     (\d+)$''', re.VERBOSE)          # Year
-# dateRegex = re.compile(r'''
-#                         ^([0-3]?[0-9])/              # Day
-#                         ([0-3]?[0-9])/              # Month
-#                         ([1-2][0-9][0-9][0-9])$      # Year
-#                         ''',re.VERBOSE)
-
-# We store months in a dictionary with the numerical value of the month key with the month 
-# name and its maximum number of days as a tuple of tuple values (to account for February leap years.)
 
 months = [
     None,
@@ -39,20 +31,6 @@ months = [
     ('November',(30,)),
     ('December',(31,))]
 
-# months = {
-#     '01':('January',(31,)),
-#     '02':('February',(28,29)),
-#     '03':('March',(31,)),
-#     '04':('April',(30,)),
-#     '05':('May',(31,)),
-#     '06':('June',(30,)),
-#     '07':('July',(31,)),
-#     '08':('August',(31,)),
-#     '09':('September',(30,)),
-#     '10':('October',(31,)),
-#     '11':('November',(30,)),
-#     '12':('December',(31,))}
-
 def is_valid_date(text):
     try:
         day, month, year = map(int,dateRegex.search(text).groups())
@@ -60,33 +38,8 @@ def is_valid_date(text):
     except (AttributeError, ValueError) as e:
         print(e)
         return False
-    
-# def _is_valid_day(day, month, year):
-#     result = False # Set the initial value of the result
-#     month_name = months[month][0] # Set the month name
-    
-#     # Get the total number of days in the given month
-#     if month_name == "February" and is_leap_year(year):
-#         month_length = months[month][1][1]
-#     else:
-#         month_length = months[month][1][0]
-
-#     if month not in months: # Test for valid month
-#         result = False
-#     elif int(month_length) < int(day): # Test for valid day
-#         result = False
-#     else:
-#         result = True
-#     return result
 
 def is_leap_year(year):
-    # year = int(year)
-    # result = False
-    # if year%100 == 0:
-    #     result = False
-    # elif year%4 == 0:
-    #     result = True
-    # return result
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
 if __name__ == '__main__':
