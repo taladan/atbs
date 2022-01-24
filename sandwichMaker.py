8733b401401809afe71cbafad8a99f7d8780b608
D.J. Crosby <taladan@gmail.com>
2022-01-24 13:33:06 -0500

    Initial commit of sandwichMaker.py
    A practice project from ATBS to work with various pyinputplus methods.  This program allows you to build a sandwich of varying types, tracks the kind of sandwich you ordered and prints the sandwich type and cost when the order is complete.

---
+++ sandwichMaker.py
@@ -0,0 +1,73 @@
+#! python
+
+# sandwichMaker.py
+
+# Automate the Boring Stuff: Chapter 8 Practice Projects
+#
+# Write a program that asks users for their sandwich preferences. 
+# The program should use PyInputPlus to ensure that they enter valid
+# input such as:
+#     * Using inputMenu() for a bread type: wheat, white, or sourdough.
+#     * Using inputMenu() for a protein type: chicken, turkey, ham, or tofu.
+#     * Using inputYesNo() to ask if they want cheese.
+#     * If so, using inputMenu() to ask for a cheese type: cheddar, Swiss, or mozzarella.
+#     * Using inputYesNo() to ask if they want mayo, mustard, lettuce, or tomato.
+#     * Using inputInt() to ask how many sandwices they want.  Make sure this number is 1 or more.
+#
+# Come up with prices for each of these options, and have your program display
+# a total cost after the user enters their selection.
+
+# Imports
+import pyinputplus as pyip
+
+# CONSTANTS
+BREADS = ['white', 'whole wheat', '9 grain', 'flatbread (white)', 'flatbread (wheat)', 'honey wheat']
+BREAD_COSTS = [.44, .60, .90, .30, .35, .64]
+PROTEINS = ['chicken', 'turkey', 'ham', 'roast beef', 'fish', 'tofu']
+PROTEIN_COSTS = [1.50, 1.00, 1.75, 2.00, 1.00, .75]
+CHEESES = ['mild cheddar', 'sharp cheddar', 'swiss', 'mozzarella', 'provalone']
+CHEESE_COSTS = [.50, .55, .75, .75, .75]
+ADDITIONS = {'mayo': .10, 'mustard': .10, 'lettuce': .5, 'tomato':.25}
+
+def sandwich_maker():
+
+    # start the order
+    print("Thank you for ordering a PythonSandwich!  Let's get your order started:\n")
+
+    # Set up some useful variables
+    sandwich_type = ""
+    total_cost = 0
+
+    # pick your bread, add it to the cost, update the type of sandwich
+    bread = pyip.inputMenu(BREADS, prompt="Please select the type of bread:\n", numbered=True)
+    total_cost += BREAD_COSTS[BREADS.index(bread)]
+    sandwich_type += f"on {bread}"
+
+    # pick your protein, add it to the cost, update the type of sandwich
+    protein = pyip.inputMenu(PROTEINS, prompt=f"Great! {bread.title()} is your bread type.  Please choose a protein:\n", numbered=True)
+    total_cost += PROTEIN_COSTS[PROTEINS.index(protein)]
+    sandwich_type = protein + ' ' + sandwich_type
+
+    # You want cheese with that?
+    wants_cheese = pyip.inputYesNo(prompt=f"Great, that's a {sandwich_type}.  Did you want cheese with that?\n")
+    if wants_cheese == 'yes':
+        cheese = pyip.inputMenu(CHEESES, prompt="Excellent choice, which type of cheese do you prefer?\n", numbered=True)
+        total_cost += CHEESE_COSTS[CHEESES.index(cheese)]
+        sandwich_type += f" with {cheese}"
+    else: 
+        print("Got it, no cheese!")
+    
+    for addon,cost in ADDITIONS.items():
+        response = pyip.inputYesNo(f"Did you want to add {addon} to that? ")
+        if response == 'yes':
+            total_cost += cost
+            sandwich_type = sandwich_type + f' add {addon}'
+        else:
+            sandwich_type = sandwich_type + f' hold the {addon}'
+
+    return (sandwich_type.capitalize(),'%.2f'%total_cost)
+
+
+if __name__ == '__main__':
+    sandwich, cost= sandwich_maker()
+    print(f"Great! You ordered a {sandwich}.  That total will be: ${cost}")
