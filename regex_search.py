9ef2281d516a02937ab8d184f5e8dadfdf147cda
D.J. Crosby <taladan@gmail.com>
2022-01-30 09:23:22 -0500

    Initial commit of regex_search.py
    This utility will search for a user-supplied regular expression against all plain text files in either the supplied directory or in the current working directory if no path is supplied. This module requires PythonMagic to be installed (pip install Python-magic) to search for plaintext mime types.

---
+++ regex_search.py
@@ -0,0 +1,82 @@
+#! python
+#
+# regex_search.py
+# Usage:
+# regex_search.py /path/to/textfiles/ expression
+#
+#   If the user includes a path to search, we search that path, otherwise
+#   we search the current working directory.  This only searches files
+#   that end in .txt
+#
+# Automate the boring stuff with python, chapter 9 exercise:
+# 
+# Write a program that opens all .txt files in a folder
+# and searches for any line that matches a user-supplied
+# regular expression.  The results should be printed
+# to the screen.
+
+# Imports
+import magic
+import os
+import re
+import sys
+from pathlib import Path
+
+usage =  '''
+Usage:
+
+    regex_search.py [path] "expression"
+
+Path is optional. If no path is included, the current
+working directory is searched. Note: the regular expression 
+searched for must be surrounded by quotes to parse correctly.
+'''
+
+previous_directory = Path.cwd()                                     # Starting directory
+mime = magic.Magic(mime=True)                                       # We will determine filetype with PythonMagic
+
+# Parse sys.argv for path and regex pattern
+if len(sys.argv) == 3 and Path(sys.argv[1]).exists():               # Path included & is valid
+    search_directory = Path(sys.argv[1])
+    try:
+        userRegex = re.compile(sys.argv[2])
+    except re.error:
+        print("Invalid regex pattern. Exiting.")
+        sys.exit()
+elif len(sys.argv) == 3 and not Path(sys.argv[1]).exists():         # Path included but not valid
+    print("Invalid input: Directory does not exist.  Exiting.")
+    sys.exit()
+elif len(sys.argv) == 2 and sys.argv[1].lower() in ['?', 'help', '--help', '-h', '-help', 'h']:      # help
+    print(usage)
+    sys.exit()
+elif len(sys.argv) == 2:                                            # No path included
+    try:
+        userRegex = re.compile(sys.argv[1]) 
+        search_directory = Path.cwd()
+    except re.error:                                                # Invalid regex
+        print("Invalid regex pattern.  Exiting.")
+        sys.exit()
+elif len(sys.argv) < 2:                                             # No path or regex included
+    print(usage)
+    sys.exit()
+
+# let's work within the directory itself.
+os.chdir(search_directory)
+
+# Gather files to iterate through or error if there are no text files
+text_files = [f for f in os.scandir(Path.cwd()) if mime.from_file(f.name) == "text/plain"]
+if len(text_files) == 0:
+    print(f"There are no text files to search in {search_directory}.  Exiting")
+    sys.exit()
+
+for f in text_files:
+    filename = f
+    f = search_directory/Path(f)
+    with open(f, encoding='utf_8') as file:
+        for line in enumerate(file.readlines()):
+            match = userRegex.search(line[1])
+            if match != None:
+                print(f"Pattern matched in file '{filename}' on line [{line[0]+1}]:> '{match.group()}'")
+
+# go back to the original directory
+os.chdir(previous_directory)
